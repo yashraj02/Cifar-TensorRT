2f4dc8e027b57a4bdeadd7dc4422affffb8c042b
Yashraj <33565924+yashraj02@users.noreply.github.com>
2020-07-16 17:29:21 +0530

    Add files via upload

--- cdac.py
+++ cdac.py
@@ -40,3 +40,26 @@ for i in range(0, 10000):
     y_gen[0].wait_to_read()
 end = time.time()
 print(time.process_time() - start)
+
+
+# Execute with TensorRT
+print('Building TensorRT engine')
+os.environ['MXNET_USE_TENSORRT'] = '1'
+arg_params.update(aux_params)
+all_params = dict([(k, v.as_in_context(mx.gpu(0))) for k, v in arg_params.items()])
+executor = mx.contrib.tensorrt.tensorrt_bind(sym, ctx=mx.gpu(0), all_params=all_params,
+                                             data=batch_shape, grad_req='null', force_rebind=True)
+# Warmup
+print('Warming up TensorRT')
+for i in range(0, 10):
+    y_gen = executor.forward(is_train=False, data=input)
+    y_gen[0].wait_to_read()
+ 
+# Timing
+print('Starting TensorRT timed run')
+start = time.process_time()
+for i in range(0, 10000):
+    y_gen = executor.forward(is_train=False, data=input)
+    y_gen[0].wait_to_read()
+end = time.time()
+print(time.process_time() - start)
